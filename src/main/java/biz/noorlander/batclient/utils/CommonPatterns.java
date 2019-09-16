cd7b9b3e86693ff3e03892216ecbdca17762ef99
Niels Noorlander <niels@mailplus.nl>
2019-09-16 15:08:04 +0200

    Issue #4: added NPC health status plugin for colored substrings

---
+++ src/main/java/biz/noorlander/batclient/utils/CommonPatterns.java
@@ -0,0 +1,7 @@
+package biz.noorlander.batclient.utils;
+
+public class CommonPatterns {
+    public static final String PATTERN_NPC_NAME = "[-A-z ',]+";
+
+    private CommonPatterns() {};
+}
---
+++ src/test/java/biz/noorlander/batclient/plugins/NPCHealthPluginTest.java
@@ -0,0 +1,31 @@
+package biz.noorlander.batclient.plugins;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NPCHealthPluginTest extends NPCHealthPlugin {
+
+    @Test
+    void shapePatternTest() {
+        loadPlugin();
+        Matcher shapeMatcher = shapePattern.matcher("Zugalo the orc king is in bad shape (25%).");
+        assertTrue(shapeMatcher.find());
+        assertEquals(5, shapeMatcher.groupCount());
+        assertEquals("bad shape", shapeMatcher.group(2));
+        assertNull(shapeMatcher.group(5));
+        shapeMatcher = shapePattern.matcher("Ulmoc, the Duke of Windham is slightly hurt (75%).");
+        assertTrue(shapeMatcher.find());
+        assertEquals("slightly hurt", shapeMatcher.group(2));
+        shapeMatcher = shapePattern.matcher("Cultist is noticeably hurt (60%) and stunned.");
+        assertTrue(shapeMatcher.find());
+        assertEquals("noticeably hurt", shapeMatcher.group(2));
+        assertEquals("stunned", shapeMatcher.group(5));
+        shapeMatcher = shapePattern.matcher("Bear is near death (5%) and *STUNNED*.");
+        assertTrue(shapeMatcher.find());
+        assertEquals("near death", shapeMatcher.group(2));
+        assertEquals("*STUNNED*", shapeMatcher.group(5));
+    }
+}
